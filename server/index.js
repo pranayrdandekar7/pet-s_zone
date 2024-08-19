e6ee9bee75bd0cb0c9b1197d9f2a315b727bab79
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-08-19 17:51:39 +0530

    mongo db is connected

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const app = express()
 app.use (express.json())
 app.use (cors())
 
+const dbConnection = async ()=>{
+    const conn =await mongoose.connect (process.env.MONGO_URL) ;
+
+    if(conn){
+        console.log(`mongoDB is connected successfully ✔`)
+    } 
+    
+}
+
+dbConnection();
+
 const PORT = process.env.PORT || 5000
 
 app.listen (PORT ,()=>{
