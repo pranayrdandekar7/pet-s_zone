b35ebfefd5dde6953119e22a053280721639b6d0
Sarita200 <saritasadgir1@gmail.com>
2024-08-21 13:38:14 +0530

    Footer added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar.js'
+import Footer from '../../components/Footer/Footer.js'
 
 function Home() {
   return (
     <div>
       <Navbar/>
       Home
+      <Footer/>
     </div>
   )
 }
