1dd125e3cf21e7ab5d238527b3fbdd295bad4039
Sarita200 <saritasadgir1@gmail.com>
2024-08-21 16:36:04 +0530

    changes in Home page

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -44,26 +44,32 @@ function SignUp() {
                     <div className="form-group">
                         <label for="exampleInputEmail1">Name</label>
                         <input type="text" className="form-control input-box" id="Full Name" aria-describedby="Full Name" placeholder="Full Name"
+                        value={user.name}
                         />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Mobile </label>
                         <input type="text" className="form-control input-box" id="Mobile" aria-describedby="Mobile" placeholder="Mobile"
+                        value={user.mobile}
                         />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Email</label>
                         <input type="Email" className="form-control input-box" id="Email" aria-describedby="Email" placeholder="Email"
+                        value={user.email}
                         />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Address</label>
                         <input type="text" className="form-control input-box" id="Address" aria-describedby="Address" placeholder="Address"
+                        value={user.address}
                         />
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Role</label >
-                        <select className="form-control input-box" id="exampleFormControlSelect1">
+                        <select className="form-control input-box" id="exampleFormControlSelect1"
+                        value={user.role}
+                        >
                             <option>Salor</option>
                             <option>Buyer</option>
                         </select>
@@ -72,6 +78,7 @@ function SignUp() {
                     <div className="form-group">
                         <label for="exampleInputEmail1">Password</label>
                         <input type="Password" className="form-control input-box" id="Password " aria-describedby="Password " placeholder="Password"
+                        value={user.password}
                         />
                     </div>
 
