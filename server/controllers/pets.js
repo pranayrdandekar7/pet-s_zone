a335946cf3ea558c35f6257c4a984f32e786b7eb
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-08-21 16:17:36 +0530

    chenges done

--- server/controllers/pets.js
+++ server/controllers/pets.js
@@ -21,38 +21,74 @@ const postPet = async (req, res) => {
         price,
 
     });
-    
-    try{
-    const savedPets = await newPet.save();
+
+    try {
+        const savedPets = await newPet.save();
+        res.json({
+            success: true,
+            message: `your pet is added successfully`,
+            data: savedPets
+
+        })
+    } catch (e) {
+        res.json({
+            success: false,
+            message: e.message,
+            data: null
+        })
+    }
+}
+
+const putPets = async (req, res) => {
+
+    const { petName,
+        age,
+        gender,
+        breed,
+        description,
+        image,
+        price } = req.body
+
+    const { id } = req.params;
+    await Pets.updateOne({ _id: id },
+        {
+            $set: {
+                petName,
+                age,
+                gender,
+                breed,
+                description,
+                image,
+                price
+
+            }
+        }
+    )
+    const updatepet = await Pets.updateOne({ id })
+
     res.json({
         success: true,
-        message: `your pet is added successfully`,
-        data: savedPets
-
+        message: `pet update successfully`,
+        data: updatepet
     })
-}catch(e){
+
+}
+
+const deletePet = async (req, res) => {
+
+    const { id } = req.params
+
+    await Pets.deleteOne({ _id: id })
     res.json({
-        success:false,
-        message:e.message,
-        data:null
+        success: true,
+        message: "Pet Delete Successfull",
+        data: null
+
     })
 }
-}
-
-const deletePet =async(req,res) => {
-
-        const {id} =req.params
-       
-      await Pets.deleteOne({_id:id})
-      res.json({
-        success:true,
-        message:"Pet Delete Successfull",
-        data:null
-      
-      })
-      }
 
 export {
     postPet,
-    deletePet
+    deletePet,
+    putPets
 }
