27b7655ea7305a96a03223dcba7ca25287035921
Sarita200 <saritasadgir1@gmail.com>
2024-08-21 13:36:58 +0530

    Header added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar.js'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar/>
+      Home
+    </div>
   )
 }
 
