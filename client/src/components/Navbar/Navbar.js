4e5aaf8156f24e7fe8b1519edd53aca1db6a8f9e
Sarita200 <saritasadgir1@gmail.com>
2024-08-21 15:00:21 +0530

    littke changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,11 +5,14 @@ import './../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function Navbar() {
     return (
         <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="/Navbar">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className='navbar-content'>
+                    <div>
+                        <a className="navbar-brand" href="/Navbar">Navbar</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item active">
@@ -26,8 +29,9 @@ function Navbar() {
                             </li>
                         </ul>
                     </div>
-                </nav>
-            
+                </div>
+            </nav>
+
         </div>
     )
 }
