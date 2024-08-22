4b66c94e37bd6a8d5678d0b448f86ca77d7ddf90
NikitaRaut21 <rautn7350@gmail.com>
2024-08-22 16:41:55 +0530

    added card details

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Home from './views/Home/Home';
 
-
-
-
-import Blog from './views/Blogs/Blog'
+import Blog from './views/Blog/Blog';
 import SignUp from './views/SignUp/SignUp';
 import Login from './views/Login/Login';
 
--- client/src/views/Blog/Blog.js
+++ client/src/views/Blog/Blog.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {useEffect,useState}from  'react';
-import toast,{Toaster} from 'react-hot-toast'
+
 import axios from 'axios'
 import BlogsCard from '../../components/BlogsCard/BlogsCard.js';
 
 function Blog() {
     const [blogs ,setBlogs]=useState([])
+    
     const loadBlogs= async ()=>{
        
-       const response  = await axios.get(`${process.env.REACT_APP_API_URL}/blog`)
-       toast.dismiss()
+       const response  = await axios.get(`${process.env.REACT_APP_API_URL}/blogs`)
+     
      
        setBlogs(response.data.data)
         }
@@ -22,8 +23,7 @@ function Blog() {
             const{ _id,title,description,category,vidUrl,date} = Blog
             return(
                 <BlogsCard   
-                       
-              key={i}
+                 key={i}
                 id={_id}
                 title={title}
                 description={description}
@@ -34,7 +34,7 @@ function Blog() {
          })
         }
 
-         <Toaster/>
+        
     </div>
     
   )
