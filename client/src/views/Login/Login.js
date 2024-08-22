7b8f744fcab4665230c1a046ceb151bcaf4f7471
Sarita200 <saritasadgir1@gmail.com>
2024-08-22 13:26:38 +0530

    axios error solved

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -10,7 +10,7 @@ function Login() {
     const [password , setPassword ] = useState('')
 
     const loginNow = async () => {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/login` , {
+        const response = await axios.post(`http://localhost:5000/login` , {
             email : email,
             password : password
         })
