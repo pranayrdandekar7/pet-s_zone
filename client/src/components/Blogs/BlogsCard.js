edcfa9d0c096121adf9a515a2a219ca3f819488c
NikitaRaut21 <rautn7350@gmail.com>
2024-08-22 11:38:28 +0530

    added components blog

---
+++ client/src/components/Blogs/BlogsCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./BlogsCard.css"
+
+function BlogsCard({ title,description,category,vidUrl,date}) {
+ 
+  
+  return (
+    <div>
+       <h1>Pets Zone Blog</h1>
+     <h1>{title}</h1>
+     <h2>{description}</h2>
+    </div>
+  )
+}
+
+export default BlogsCard
