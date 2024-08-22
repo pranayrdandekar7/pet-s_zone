0c9398c1a6ee60e64abc40e3fc9cc2696a19d1e8
NikitaRaut21 <rautn7350@gmail.com>
2024-08-22 11:26:05 +0530

    added blog  models

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Home from './views/Home/Home';
+import Blog from './views/Blogs/Blog'
 
 
 const router= createBrowserRouter ([
   {
     path:"/",
     element:<Home/>
+  },
+  {
+    path:"/blog",
+    element:<Blog/>
   },
   {
     path:"*",
