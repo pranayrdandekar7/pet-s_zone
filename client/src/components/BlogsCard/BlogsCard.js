5b3ca149a733f05244f4b2f748a790bc048a3730
NikitaRaut21 <rautn7350@gmail.com>
2024-08-23 15:41:46 +0530

    added  blogcard

--- client/src/components/BlogsCard/BlogsCard.js
+++ client/src/components/BlogsCard/BlogsCard.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import "./BlogsCard.css"
 
-function BlogsCard({ title,description,category,vidUrl,date}) {
+function BlogsCard({ title,description,category,vidUrl,createdAt}) {                          
  
   
   return (
+    
+    <div className='main-card'>
     <div className='blog-card'>
-       <h1>Pets Zone Blog</h1>
-     <h3>{title}</h3>
-     <h4>{description}</h4>
-     <p>{vidUrl}
-      
-     </p>
-    </div>
+     <p className='blog-title'>{title}</p>
+      <p className='video-url'>{vidUrl}</p>
+      <p className='blog-category'>{category}</p>
+     <p className='blog-desc'>{description}</p>
+     <p className='blog-date'>{new Date (createdAt).toLocaleString()}</p>
+     </div>
+     </div>
+    
   )
 }
 
