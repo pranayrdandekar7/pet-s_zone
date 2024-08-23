c7c3eaa8e6981509c0aeb264bddc4dede503d72b
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-08-23 11:28:15 +0530

    pet details add

--- client/src/components/animal_cards/animal_cards.js
+++ client/src/components/animal_cards/animal_cards.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../animal_cards/animal_cards.css'
 function Animal_cards({ Img, name, }) {
     return (
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Home from './views/Home/Home';
 import Blog from './views/Blogs/Blog'
+import PetDatails from "./views/PetDatails/PetDatails"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const router= createBrowserRouter ([
@@ -14,6 +16,10 @@ const router= createBrowserRouter ([
   {
     path:"/blog",
     element:<Blog/>
+  },
+  {
+    path:"/petdetails",
+    element:<PetDatails/>
   },
   {
     path:"*",
